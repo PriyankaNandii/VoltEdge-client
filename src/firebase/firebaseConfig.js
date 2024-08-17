2ea5660c168e04df7054dc4939ca028b119a1d46
Priyanka Nandii <priyanka15-11155@diu.edu.bd>
2024-08-17 18:43:48 +0600

    category filter

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,8 +3,7 @@
 
 
 
-//   apiKey: import.meta.env.VITE_APIKEY,
-//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+
 //   projectId: import.meta.env.VITE_PROJECTID,
 //   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
 //   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
